8d0ddf709f057e855ebf4b17139d57e0edb61516
LANIT\DUsachev <dusachev@lanit.ru>
2019-08-01 13:39:36 +0300

    fix linint python tests

--- molecule/gce_test/tests/test_default.py
+++ molecule/gce_test/tests/test_default.py
@@ -2,7 +2,8 @@ import os
 
 import testinfra.utils.ansible_runner
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+	os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
 # check if MongoDB is enabled and running
